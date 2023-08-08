b836b1c75d0cf876da2892902883cee713b00257
Jeremy Cantrell <jmcantrell@gmail.com>
2023-08-08 13:36:55 -0500

    Add unit tests for solver

--- puzzles/day02/src/answer/solve.rs
+++ puzzles/day02/src/answer/solve.rs
@@ -14,3 +14,34 @@ pub fn solve1(parsed: &Parsed1) -> anyhow::Result<Solution1> {
 pub fn solve2(parsed: &Parsed2) -> anyhow::Result<Solution2> {
     Ok(parsed.iter().map(|present| present.ribbon_length()).sum())
 }
+
+#[cfg(test)]
+pub mod tests {
+    use crate::answer::{parse1, parse2};
+
+    #[test]
+    fn solve1() -> anyhow::Result<()> {
+        macro_rules! test {
+            ($input:expr, $expected:expr) => {
+                assert_eq!(super::solve1(&parse1($input)?)?, $expected);
+            };
+        }
+
+        test!("2x3x4\n1x1x10\n", 58 + 43);
+
+        Ok(())
+    }
+
+    #[test]
+    fn solve2() -> anyhow::Result<()> {
+        macro_rules! test {
+            ($input:expr, $expected:expr) => {
+                assert_eq!(super::solve2(&parse2($input)?)?, $expected);
+            };
+        }
+
+        test!("2x3x4\n1x1x10\n", 34 + 14);
+
+        Ok(())
+    }
+}
