138e1d59c78cce7e6edf234f4f9124c73de90cca
Jeremy Cantrell <jmcantrell@gmail.com>
2023-08-19 02:30:08 -0500

    Add solution for day 11

---
+++ puzzles/day11/src/answer/solve.rs
@@ -0,0 +1,86 @@
+use std::collections::HashSet;
+
+use anyhow::Context;
+
+use crate::core::Password;
+
+use super::{Parsed1, Parsed2};
+
+type Solution = String;
+pub type Solution1 = Solution;
+pub type Solution2 = Solution;
+
+fn iter_valid_passwords(start: &Password) -> impl Iterator<Item = Password> {
+    let letters: Vec<_> = ('a'..='z').collect();
+    let straights: HashSet<_> = letters.windows(3).map(|window| window.to_vec()).collect();
+    let confusing: HashSet<_> = ['i', 'o', 'l'].into_iter().collect();
+    let pairs: HashSet<_> = letters.iter().map(|&c| [c, c]).collect();
+
+    let mut passwords = start.iter();
+
+    std::iter::from_fn(move || loop {
+        let password = passwords.next().unwrap();
+
+        if let Some(i) = password.chars().position(|c| confusing.contains(c)) {
+            passwords.advance_index(i);
+            continue;
+        }
+
+        if !password
+            .as_ref()
+            .windows(3)
+            .any(|window| straights.contains(window))
+        {
+            continue;
+        }
+
+        if pairs
+            .iter()
+            .filter(|pair| password.as_ref().windows(2).any(|window| &window == pair))
+            .take(2)
+            .count()
+            < 2
+        {
+            continue;
+        }
+
+        break Some(password);
+    })
+}
+
+pub fn solve1(parsed: &Parsed1) -> anyhow::Result<Solution1> {
+    iter_valid_passwords(parsed)
+        .next()
+        .map(|password| password.to_string())
+        .context("no password")
+}
+
+pub fn solve2(parsed: &Parsed2) -> anyhow::Result<Solution2> {
+    iter_valid_passwords(parsed)
+        .nth(1)
+        .map(|password| password.to_string())
+        .context("no password")
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::core::Password;
+
+    #[test]
+    fn iter_valid_passwords() {
+        macro_rules! test {
+            ($input:expr, $expected:expr) => {
+                assert_eq!(
+                    super::iter_valid_passwords(&Password::try_from($input).unwrap())
+                        .next()
+                        .unwrap()
+                        .to_string(),
+                    $expected
+                );
+            };
+        }
+
+        test!("abcdefgh", "abcdffaa");
+        test!("ghijklmn", "ghjaabcc");
+    }
+}
---
+++ puzzles/day11/src/core/password.rs
@@ -0,0 +1,155 @@
+use anyhow::{bail, ensure};
+
+use std::convert::TryFrom;
+
+const PASSWORD_LEN: usize = 8;
+
+type Inner = Vec<char>;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Password(Inner);
+
+impl Password {
+    pub fn iter(&self) -> PasswordIter {
+        PasswordIter::after(self.clone())
+    }
+
+    pub fn chars(&self) -> impl Iterator<Item = &char> {
+        self.0.iter()
+    }
+}
+
+impl From<Password> for Inner {
+    fn from(password: Password) -> Self {
+        password.0
+    }
+}
+
+impl AsRef<Inner> for Password {
+    fn as_ref(&self) -> &Inner {
+        &self.0
+    }
+}
+
+impl std::fmt::Display for Password {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for c in self.0.iter() {
+            write!(f, "{}", c)?;
+        }
+        Ok(())
+    }
+}
+
+impl TryFrom<&str> for Password {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        ensure!(
+            s.len() == PASSWORD_LEN,
+            "expected password to be {} characters, but it was {}",
+            PASSWORD_LEN,
+            s.len()
+        );
+
+        let chars: Vec<_> = s.chars().collect();
+
+        for (i, c) in chars.iter().enumerate() {
+            if !(c.is_alphabetic() && c.is_lowercase()) {
+                bail!(
+                    "character number {} is not a lowercase letter: {:?}",
+                    i + 1,
+                    c
+                );
+            }
+        }
+
+        Ok(Self(chars))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PasswordIter(Inner);
+
+fn next_char(c: char) -> (char, bool) {
+    if c == 'z' {
+        ('a', true)
+    } else {
+        (char::from_u32(c as u32 + 1).unwrap(), false)
+    }
+}
+
+fn increment_letters(letters: &mut Inner, at: usize) {
+    let at = at.clamp(0, letters.len());
+
+    for i in (0..=at).rev() {
+        let current = letters.get_mut(i).unwrap();
+        let (next, carry) = next_char(*current);
+        *current = next;
+        if !carry {
+            break;
+        }
+    }
+}
+
+impl PasswordIter {
+    pub fn after(password: Password) -> Self {
+        Self(password.into())
+    }
+
+    pub fn advance_index(&mut self, i: usize) {
+        increment_letters(&mut self.0, i);
+        for j in (i + 1)..self.0.len() {
+            self.0[j] = 'a';
+        }
+    }
+}
+
+impl Iterator for PasswordIter {
+    type Item = Password;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let i = self.0.len() - 1;
+        increment_letters(&mut self.0, i);
+        Some(Password(self.0.clone()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn password_display() {
+        assert_eq!(
+            Password::try_from("abcdefgh").unwrap().to_string(),
+            "abcdefgh"
+        );
+    }
+
+    #[test]
+    fn password_ensures_len() {
+        assert!(Password::try_from("abc").is_err());
+        assert!(Password::try_from("abcdefghi").is_err());
+    }
+
+    #[test]
+    fn password_ensures_lowercase_letters() {
+        assert!(Password::try_from("ABCDEFGH").is_err());
+    }
+
+    #[test]
+    fn password_iter_after() {
+        assert_eq!(
+            PasswordIter::after(Password::try_from("aaaaaaaa").unwrap()).next(),
+            Some(Password::try_from("aaaaaaab").unwrap())
+        );
+        assert_eq!(
+            PasswordIter::after(Password::try_from("aaaaazzz").unwrap()).next(),
+            Some(Password::try_from("aaaabaaa").unwrap())
+        );
+        assert_eq!(
+            PasswordIter::after(Password::try_from("zzzzzzzz").unwrap()).next(),
+            Some(Password::try_from("aaaaaaaa").unwrap())
+        );
+    }
+}
