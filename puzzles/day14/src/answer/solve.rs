7b8cf241f6dfabe5c5d858b88cca7193a23bc0ba
Jeremy Cantrell <jmcantrell@gmail.com>
2023-08-21 01:00:20 -0500

    Add solution for day 14

---
+++ puzzles/day14/src/answer/solve.rs
@@ -0,0 +1,65 @@
+use anyhow::{ensure, Context};
+
+use super::{Parsed1, Parsed2};
+
+type Solution = usize;
+pub type Solution1 = Solution;
+pub type Solution2 = Solution;
+
+#[cfg(not(test))]
+const TIME_LIMIT: usize = 2503;
+
+#[cfg(test)]
+const TIME_LIMIT: usize = 1000;
+
+pub fn solve1(parsed: &Parsed1) -> anyhow::Result<Solution1> {
+    parsed
+        .iter()
+        .map(|reindeer| reindeer.distance_traveled_after(TIME_LIMIT))
+        .max()
+        .context("no reindeer")
+}
+
+pub fn solve2(parsed: &Parsed2) -> anyhow::Result<Solution2> {
+    ensure!(!parsed.is_empty(), "no reindeer");
+
+    let mut points = vec![0usize; parsed.len()];
+    let mut distances = vec![0usize; parsed.len()];
+
+    for duration in 1..=TIME_LIMIT {
+        for (i, reindeer) in parsed.iter().enumerate() {
+            distances[i] = reindeer.distance_traveled_after(duration);
+        }
+
+        let max_distance = distances.iter().max().unwrap();
+
+        for (i, distance) in distances.iter().enumerate() {
+            if distance == max_distance {
+                points[i] += 1;
+            }
+        }
+    }
+
+    Ok(points.into_iter().max().unwrap())
+}
+
+#[cfg(test)]
+pub mod tests {
+    use aoc::Input;
+
+    use crate::answer::{parse1, parse2};
+
+    const INPUT: Input = include_str!("../../input-test.txt");
+
+    #[test]
+    fn solve1() -> anyhow::Result<()> {
+        assert_eq!(super::solve1(&parse1(INPUT)?)?, 1120);
+        Ok(())
+    }
+
+    #[test]
+    fn solve2() -> anyhow::Result<()> {
+        assert_eq!(super::solve2(&parse2(INPUT)?)?, 689);
+        Ok(())
+    }
+}
---
+++ puzzles/day14/src/core/reindeer.rs
@@ -0,0 +1,79 @@
+use std::convert::TryFrom;
+
+use anyhow::Context;
+use lazy_regex::{lazy_regex, Lazy, Regex};
+
+#[derive(Debug, Clone)]
+pub struct Reindeer<'a> {
+    pub name: &'a str,
+    pub top_speed: usize,
+    pub fly_duration: usize,
+    pub rest_duration: usize,
+}
+
+impl<'a> Reindeer<'a> {
+    pub fn new(name: &'a str, top_speed: usize, fly_duration: usize, rest_duration: usize) -> Self {
+        Self {
+            name,
+            top_speed,
+            fly_duration,
+            rest_duration,
+        }
+    }
+
+    pub fn distance_traveled_after(&self, total_duration: usize) -> usize {
+        let period_duration = self.fly_duration + self.rest_duration;
+
+        let whole_periods = total_duration / period_duration;
+        let remaining_seconds = total_duration % period_duration;
+
+        self.top_speed
+            * (whole_periods * self.fly_duration + self.fly_duration.min(remaining_seconds))
+    }
+}
+
+static RE: Lazy<Regex> = lazy_regex!(
+    r"^(?<name>\S+) can fly (?<top_speed>\d+) km/s for (?<fly_duration>\d+) seconds, but then must rest for (?<rest_duration>\d+) seconds\."
+);
+
+impl<'a> TryFrom<&'a str> for Reindeer<'a> {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        let captures = RE
+            .captures(s)
+            .context(format!("expected input to match: {:?}", RE.as_str()))?;
+
+        let name = captures.name("name").unwrap().as_str();
+        let top_speed = captures["top_speed"].parse()?;
+        let fly_duration = captures["fly_duration"].parse()?;
+        let rest_duration = captures["rest_duration"].parse()?;
+
+        Ok(Self {
+            name,
+            top_speed,
+            fly_duration,
+            rest_duration,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn distance_traveled_after() {
+        macro_rules! test {
+            ($name:expr, $speed:expr, $fly:expr, $rest:expr, $duration:expr, $expected:expr) => {
+                assert_eq!(
+                    Reindeer::new($name, $speed, $fly, $rest).distance_traveled_after($duration),
+                    $expected
+                );
+            };
+        }
+
+        test!("Comet", 14, 10, 127, 1000, 1120);
+        test!("Dancer", 16, 11, 162, 1000, 1056);
+    }
+}
