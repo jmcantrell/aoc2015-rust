240f4bff6c53be635ddb86597b8e9217dd311dea
Jeremy Cantrell <jmcantrell@gmail.com>
2023-08-24 15:12:59 -0500

    Add solution for day 17

---
+++ puzzles/day17/src/answer/solve.rs
@@ -0,0 +1,56 @@
+use itertools::Itertools;
+
+use crate::core::ContainerSize;
+
+use super::{Parsed1, Parsed2};
+
+type Solution = usize;
+pub type Solution1 = Solution;
+pub type Solution2 = Solution;
+
+#[cfg(test)]
+const TARGET: usize = 25;
+
+#[cfg(not(test))]
+const TARGET: usize = 150;
+
+fn find_containers(
+    containers: &[ContainerSize],
+    target: ContainerSize,
+) -> impl Iterator<Item = Vec<&ContainerSize>> {
+    containers
+        .iter()
+        .powerset()
+        .filter(move |containers| containers.iter().cloned().sum::<ContainerSize>() == target)
+}
+
+pub fn solve1(parsed: &Parsed1) -> anyhow::Result<Solution1> {
+    Ok(find_containers(parsed, TARGET).count())
+}
+
+pub fn solve2(parsed: &Parsed2) -> anyhow::Result<Solution2> {
+    Ok(find_containers(parsed, TARGET)
+        .min_set_by_key(|set| set.len())
+        .len())
+}
+
+#[cfg(test)]
+pub mod tests {
+    use aoc::Input;
+
+    use crate::answer::{parse1, parse2};
+
+    const INPUT: Input = include_str!("../../input-test.txt");
+
+    #[test]
+    fn solve1() -> anyhow::Result<()> {
+        assert_eq!(super::solve1(&parse1(INPUT)?)?, 4);
+        Ok(())
+    }
+
+    #[test]
+    fn solve2() -> anyhow::Result<()> {
+        assert_eq!(super::solve2(&parse2(INPUT)?)?, 3);
+        Ok(())
+    }
+}
---
+++ puzzles/day17/src/core/mod.rs
@@ -0,0 +1,1 @@
+pub type ContainerSize = usize;
